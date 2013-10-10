3e6dd2919b0c732cbed0925faa79ead6f0309560
zknicker <zknicker@gmail.com>
2013-10-10 18:13:16 -0400

    Added documentation, small code cleanup.

--- src/Database/EventFinder.java
+++ src/Database/EventFinder.java
@@ -8,14 +8,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
+/**
+ * Finds rows of events in the database for different critera. In all cases, the row object model,
+ * EventGateway, is returned either as a single object or a list.
+ */
 public class EventFinder {
-	
-	public EventFinder()
-	{}
-	
 
+    /**
+     * Finds an event in the database with the specified event ID.
+     *
+     * @param eventId - the event ID of the desired event.
+     * @return the event with the desired event Id.
+     * @throws SQLException on error querying the database.
+     */
 	public EventGateway findEvent(long eventId) throws SQLException {
 		
 		Connection connection = DatabaseConnection.getDatabaseConnection();
@@ -24,14 +29,20 @@ public class EventFinder {
         ResultSet results = statement.executeQuery("SELECT * FROM EVENTS WHERE EVENT_ID = " + eventId) ;
         results.next();
        
-        EventGateway eventGateway = EventGateway.load(results);
+        EventGateway eventGateway = EventGateway.create(results);
         statement.close();
         
         return eventGateway;
-   
 	}
-	
-	
+
+    /**
+     * Finds all events with a given category (case sensitive).
+     * TODO - remove case sensitivity requirement.
+     *
+     * @param category - the desired category.
+     * @return the events with the desired category.
+     * @throws SQLException on error querying the database.
+     */
 	public List<EventGateway> findEventsByCategory(String category) throws SQLException {
 		
 		Connection connection = DatabaseConnection.getDatabaseConnection();
@@ -42,8 +53,8 @@ public class EventFinder {
        
         while (results.next())
         {
-        EventGateway eventGateway = EventGateway.load(results);
-        result.add(eventGateway);
+            EventGateway eventGateway = EventGateway.create(results);
+            result.add(eventGateway);
         }
         statement.close();
    
