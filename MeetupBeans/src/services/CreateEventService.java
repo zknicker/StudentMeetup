bffe0e1b0f2809853bb83ac51a793ca1f564388c
zknicker <zknicker@gmail.com>
2013-11-04 23:00:19 -0500

    Fancy design stuff paving the way for a more properly constructed, navigable site.

--- MeetupBeans/src/services/CreateEventService.java
+++ MeetupBeans/src/services/CreateEventService.java
@@ -2,11 +2,8 @@ package services;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.*;
-
 import entities.*;
-
 import javax.ejb.Stateless;
 
 @Stateless
---
+++ MeetupBeans/src/services/ViewEventService.java
@@ -0,0 +1,12 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import entities.*;
+import javax.ejb.Stateless;
+
+@Stateless
+public class ViewEventService {
+
+}
